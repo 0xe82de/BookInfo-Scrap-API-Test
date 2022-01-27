bbcce6f45ca1a7ea012292b7cd192357e3aaadb0
김경석 <0xe82de@gmail.com>
2022-01-27 23:40:19 +0900

    Feat: 책 정보 클래스 추가

---
+++ src/main/java/com/e82de/bookinfoscrap/interpark/InterparkBookInfo.java
@@ -0,0 +1,50 @@
+package com.e82de.bookinfoscrap.interpark;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+public class InterparkBookInfo {
+
+    private final String isbn;
+
+    private final String title;
+
+    private final String author;
+
+    private final String description;
+
+    private final Integer price;
+
+    private final String smallImgUrl;
+
+    private final String largeImgUrl;
+
+    private final Integer categoryId;
+
+    private final String categoryName;
+
+    private final String publisher;
+
+    private final LocalDateTime publicationDate;
+
+    @Builder
+    public InterparkBookInfo(String isbn, String title, String author, String description, Integer price, String smallImgUrl, String largeImgUrl, Integer categoryId, String categoryName, String publisher, LocalDateTime publishDate) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.price = price;
+        this.smallImgUrl = smallImgUrl;
+        this.largeImgUrl = largeImgUrl;
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.publisher = publisher;
+        this.publicationDate = publishDate;
+    }
+
+}
